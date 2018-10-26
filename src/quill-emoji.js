4f3312555e66a3c602f08af38bc31c647257a08c
Олег Бочагов <w.iohansson@gmail.com>
2018-10-26 18:50:11 +0300

    dont use ShortNameEmoji for now

--- src/quill-emoji.js
+++ src/quill-emoji.js
@@ -1,15 +1,17 @@
 import Quill from 'quill';
 import EmojiBlot from './format-emoji-blot';
-import ShortNameEmoji from './module-emoji';
+// ShortNameEmoji messes up typing some symbols
+// don't need it till Quill 2.0, then maybe
+// import ShortNameEmoji from './module-emoji';
 import ToolbarEmoji from './module-toolbar-emoji';
 import TextAreaEmoji from './module-textarea-emoji';
 import './scss/quill-emoji.scss';
 
 Quill.register({
     'formats/emoji': EmojiBlot,
-    'modules/emoji-shortname': ShortNameEmoji,
+    // 'modules/emoji-shortname': ShortNameEmoji,
     'modules/emoji-toolbar': ToolbarEmoji,
     'modules/emoji-textarea': TextAreaEmoji
-  }, true);  
+  }, true);
 
-export default { EmojiBlot, ShortNameEmoji, ToolbarEmoji, TextAreaEmoji };
+export default { EmojiBlot, /* ShortNameEmoji, */ ToolbarEmoji, TextAreaEmoji };
